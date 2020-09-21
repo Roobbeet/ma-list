f9035dc84b6bdf76787948edf5bfa6bd2f7c78be
Robeet <sukocorobertus@gmail.com>
2020-09-21 20:00:42 +0700

    v.1.0

--- src/components/list/list.component.jsx
+++ src/components/list/list.component.jsx
@@ -3,11 +3,11 @@ import './list.styles.css'
 
 const ListItem = ({item, isDone, handleDone}) => {
     return(
-        <div className="item-container" onClick={() => handleDone(item)}>
+        <div className={`item-container ${isDone ? 'done-task' : ''}`} onClick={() => handleDone(item)}>
             <h2 className={`item-name ${isDone ? 'done-task' : ''}`} >{item}</h2>
-            <div className="remove-button" ><i className="far fa-times-circle"></i></div>
+            <div className={`remove-button ${isDone ? 'hidden' : ''}`} ><i className="far fa-times-circle"></i></div>
         </div>
     )
 }
 
-export default ListItem;
+export default ListItem; 
